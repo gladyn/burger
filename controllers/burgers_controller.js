038e2c2467bd222d2c978e12b9e2948bfbf6b309
gladyn <glds.nvrr@gmail.com>
2019-03-26 23:33:46 -0700

    updates

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,44 @@
+
+var express = require("express");
+
+var router = express.Router();
+
+// Import the model 
+var burger = require("../models/burger.js");
+
+//GET
+
+router.get("/", function(req, res) {
+  burger.selectAll(function(data) {
+    var hbsObject = {
+      burgers: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
+});
+
+//POST insertOne 
+router.post("/", function(req, res) {
+  burger.insertOne([
+    "burger_name"], [
+    req.body.name], function() {
+    res.redirect("/");
+  });
+});
+
+//PUT updateOne 
+router.put("/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  console.log("condition", condition);
+
+  burger.updateOne({
+    devoured: req.body.devoured
+  }, condition, function() {
+    res.redirect("/");
+  });
+});
+
+
+module.exports = router;
